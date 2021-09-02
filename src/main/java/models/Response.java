b91966ed3c40e41240c3e98515086ded303f4378
Brittany Wheeler-Wolowicz <nannyhap@icloud.com>
2021-09-02 14:25:39 -0500

    Models setup

---
+++ src/main/java/models/Response.java
@@ -0,0 +1,54 @@
+package models;
+
+public class Response {
+    private Long id;
+    private Long ticketId;
+    private Long responseTypeId;
+
+    private String message;
+    private String responseType;
+
+    public Response() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(Long ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public Long getResponseTypeId() {
+        return responseTypeId;
+    }
+
+    public void setResponseTypeId(Long responseTypeId) {
+        this.responseTypeId = responseTypeId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getResponseType() {
+        return responseType;
+    }
+
+    public void setResponseType(String responseType) {
+        this.responseType = responseType;
+    }
+}
