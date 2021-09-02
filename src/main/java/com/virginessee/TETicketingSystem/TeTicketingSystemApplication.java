788f2fe83c8b34d546cef8aeb94f838e9176a521
russellhimes <william.himes@richmond.edu>
2021-09-02 15:00:48 -0400

    first commit

---
+++ src/main/java/com/virginessee/TETicketingSystem/TeTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.virginessee.TETicketingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeTicketingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/virginessee/TETicketingSystem/TeTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virginessee.TETicketingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeTicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
