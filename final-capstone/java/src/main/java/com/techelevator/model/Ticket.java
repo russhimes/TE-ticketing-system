4d13a21cd2681bed4e6093380588c552cf84e096
Russell Himes <william.himes@richmond.edu>
2021-10-13 10:23:01 -0400

    adding security code

---
+++ final-capstone/java/src/main/java/com/techelevator/model/Ticket.java
@@ -0,0 +1,125 @@
+package com.virginessee.TETicketingSystem.models;
+
+import java.time.LocalDate;
+
+public class Ticket {
+    private Long id;
+
+    private Long customerId;
+    private Long typeId;
+    private Long statusId;
+    private Long caseId;
+    private Long departmentId;
+    // add technician id instead of department id?
+
+    private String title;
+    private String description;
+
+    // May want to add a file field here so that we can save the screenshot as a png file. Url will the the object stored in the database.
+    private String screenshotUrl;
+    // might want a status description instead of just a status id, remove whichever one we don't use. Same with type.
+    private String type;
+    private String status;
+    // are we omitting this or no due to complexity? 9.16.21
+    private LocalDate timestamp;
+
+    public Ticket() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Long getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Long typeId) {
+        this.typeId = typeId;
+    }
+
+    public Long getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(Long statusId) {
+        this.statusId = statusId;
+    }
+
+    public Long getCaseId() {
+        return caseId;
+    }
+
+    public void setCaseId(Long caseId) {
+        this.caseId = caseId;
+    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getScreenshotUrl() {
+        return screenshotUrl;
+    }
+
+    public void setScreenshotUrl(String screenshotUrl) {
+        this.screenshotUrl = screenshotUrl;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDate timestamp) {
+        this.timestamp = timestamp;
+    }
+}
